3b2a3409d12729e339b053b105c1ebf416ba3bb2
msrocka <michael.srocka@gmail.com>
2016-11-18 14:36:09 +0100

    added benchmark for process table

--- src/main/java/org/openlca/core/benchmarks/ProcessTableTest.java
+++ src/main/java/org/openlca/core/benchmarks/ProcessTableTest.java
@@ -14,14 +14,14 @@ import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
 import org.openlca.core.database.IDatabase;
-import org.openlca.core.database.NativeSql;
 import org.openlca.core.database.derby.DerbyDatabase;
 import org.openlca.core.matrix.cache.FlowTypeTable;
+import org.openlca.core.matrix.cache.ProcessTable;
 
 @BenchmarkMode(Mode.AverageTime)
 @OutputTimeUnit(TimeUnit.SECONDS)
 @State(Scope.Benchmark)
-public class ExchangeTableJoin {
+public class ProcessTableTest {
 
 	private IDatabase db;
 
@@ -36,33 +36,19 @@ public class ExchangeTableJoin {
 	}
 
 	@Benchmark
-	public void scanWithFlowTypeTable() throws Exception {
+	public void processTable() {
 		FlowTypeTable flowTypes = FlowTypeTable.create(db);
-		String query = "SELECT f_owner, f_flow, resulting_amount_value " +
-				"FROM tbl_exchanges";
-		NativeSql.on(db).query(query, r -> {
-			long flowId = r.getLong(1);
-			flowTypes.get(flowId);
-			return true;
-		});
-	}
-
-	@Benchmark
-	public void scanWithJoin() throws Exception {
-		String query = "SELECT e.f_owner, e.f_flow, e.resulting_amount_value"
-				+ " FROM tbl_exchanges e INNER JOIN tbl_flows f ON e.f_flow = f.id"
-				+ " WHERE  (f.flow_type = 'PRODUCT_FLOW' AND e.is_input = 0)"
-				+ " OR (f.flow_type = 'WASTE_FLOW' AND e.is_input = 1)";
-		NativeSql.on(db).query(query, r -> true);
+		ProcessTable.create(db, flowTypes);
 	}
 
 	public static void main(String[] args) throws Exception {
 		Options opt = new OptionsBuilder()
-				.include(ExchangeTableJoin.class.getName())
+				.include(ProcessTableTest.class.getName())
 				.warmupIterations(2)
-				.measurementIterations(5)
+				.measurementIterations(10)
 				.forks(1)
 				.build();
 		new Runner(opt).run();
 	}
+
 }
