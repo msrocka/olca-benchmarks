8ce9c46a6dc652031a554df1ca22858a29afe146
msrocka <michael.srocka@gmail.com>
2018-07-10 16:09:40 +0200

    update project config

--- src/main/java/org/openlca/core/benchmarks/Config.java
+++ src/main/java/org/openlca/core/benchmarks/Config.java
@@ -2,6 +2,6 @@ package org.openlca.core.benchmarks;
 
 public interface Config {
 
-	String DB_PATH = "C:\\Users\\Besitzer\\openLCA-data-1.4\\databases\\ecoinvent_3_2_apos";
+	String DB_PATH = "C:\\Users\\Besitzer\\openLCA-data-1.4\\databases\\ecoinvent_34_apos_unit_20180420";
 
 }
